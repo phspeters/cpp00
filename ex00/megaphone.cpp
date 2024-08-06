33632c6329f0d64a74a9e7056dad11ca55653dcc
Peters <peters@archlinux>
2024-08-05 21:03:53 -0300

    improved ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <cctype>
 
-#define PRINT(x) std::cout << x << std::endl
+void	print(std::string str)
+{
+	std::cout << str << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     if (argc == 1)
 	{
-		PRINT("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
-		return (0);
+		print("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+		return 0;
 	}
 
 	std::string input;
 	for (int i = 1; i < argc; i++)
-		input += (argv[i]);
+		input += argv[i];
 	
 	for (std::string::size_type i = 0; i < input.length(); i++)
 		input[i] = std::toupper(input[i]);
 
-	PRINT(input);
+	print(input);
 
     return 0;
 }
